690d581448ae8417a727e257717593fa7e0c15cc
liu <liu@XB-20170301OMYB>
2017-03-19 09:03:12 +0800

    creat

---
+++ src/main/java/com/bbdj8/bus/utils/Constant.java
@@ -0,0 +1,11 @@
+package com.bbdj8.bus.utils;
+
+/**
+ * 常量类
+ * @author liwenjun
+ *
+ */
+public class Constant {
+	public static String SMS_MESSAGE="你有新建快件在宝贝到家了，快来取件吧。【宝贝到家】";
+	public static String WEIXIN_MESSAGE="";
+}
---
+++ src/main/java/com/bbdj8/bus/utils/MessageUtil.java
@@ -0,0 +1,19 @@
+package com.bbdj8.bus.utils;
+
+/**
+ * 消息工具类
+ * @author liwenjun
+ *
+ */
+public class MessageUtil {
+	/**
+	 * 发送短信
+	 * @param tel
+	 * @param msg
+	 * @return
+	 */
+	public static String sendSms(String tel,String msg){
+		
+		return "";
+	}
+}
